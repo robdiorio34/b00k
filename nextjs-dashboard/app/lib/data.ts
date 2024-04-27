006c9c81a0e83112c32dabd11e2605b88120ba03
robdiorio34 <54613383+robdiorio34@users.noreply.github.com>
2024-04-27 14:53:36 -0400

    Added URL param search + pagination for invoices

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -15,20 +15,17 @@ import { unstable_noStore as noStore } from 'next/cache';
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  //prevents from using cached data over up to date, dynamic data coming in
   noStore();
 
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     console.log("Revenue Data: ", data.rows);
 
-    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
